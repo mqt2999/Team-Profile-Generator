5024bdce507fe78969e633085ca4dc64528d08ae
Marquell Thompson <mqt060@gmial.com>
2021-04-13 10:41:16 -0500

    adding manager class

---
+++ Develope/lib/Manager.js
@@ -0,0 +1,19 @@
+//importing the parent class Employee 
+const Employee = require('./Employee')
+//Manager class that extends Employee
+class Manager extends Employee {
+    //constructor reads in class information
+    constructor(name,id,email,officeNumber){
+        //passes name id email to parent class
+        super(name,id,email)
+        this.officeNumber = officeNumber;
+    }
+    // method returns office number 
+    getOfficeNumber(){
+        return this.officeNumber;
+    }
+    // method overrides the parent class and returns the role
+    getRole(){
+        return 'Manager';
+    }
+}
