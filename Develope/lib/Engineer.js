93413818717cb7c606c3ec19080cece807114240
Marquell Thompson <mqt060@gmial.com>
2021-04-13 10:29:57 -0500

    adding the engineer class

---
+++ Develope/lib/Engineer.js
@@ -0,0 +1,18 @@
+//importing the parent class Employee
+const Employee = requier('./Employee')
+//Engineer class that extends parent class Employee
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        //passes the name id and email to the parent class 
+        super(name, id, email)
+        this.github = github;
+    }
+    //returns the github name 
+    getGithub() {
+        return this.github;
+    }
+    //overrides the parent method and returns Engineer
+    getRole() {
+        return 'Engineer';
+    }
+}
